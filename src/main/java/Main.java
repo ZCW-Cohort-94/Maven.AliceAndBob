5d131f973f171bbbe4674306e54fff33c6381698
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-02 13:16:37 -0500

    I have made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.*;
 public class Main {
 
     public static void main(String[] args ){
 
+        String input;
+        Scanner userIn = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        input = userIn.next();
+        userIn.close();
+        System.out.println(aliceBob(input));
+
     }
+
+    static String aliceBob(String name) {
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            return "Hello " + name.substring(0,1).toUpperCase() + name.substring(1);
+        } else {
+            return "You are not Alice or Bob";
+        }
+    }
+
 }
