9b2727566e5c629d47278ed0056f29fcfb3bde4f
dhannyamohangeetha <dhannyabrijesh.ktm@gmail.com>
2023-03-04 08:11:50 -0500

    Alice and Bob Project completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        checkAndGreetUser();
+    }
 
+    private static void checkAndGreetUser() {
+        Scanner myObj = new Scanner(System.in);
+        String userName;
+        System.out.println("Enter username");
+        userName = myObj.nextLine();
+        if (userName.equals("Alice")) {
+            System.out.println("Hello Alice");
+        } else if (userName.equals("Bob")) {
+            System.out.println("Hello Bob");
+        }
     }
 }
+
